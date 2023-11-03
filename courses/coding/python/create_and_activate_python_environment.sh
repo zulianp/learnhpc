a8670c99489035695e031d5de04b169d4a3e692c
Patrick Zulian <patrick.zulian@gmail.com>
2023-11-03 16:00:00 +0100

    Added env script

---
+++ courses/coding/python/create_and_activate_python_environment.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Begin script
+
+# Exit if an error is encountered
+set -e
+
+# Print every command to the command line
+set -x
+
+# Create a python virtual environment (venv)
+python3 -m venv venv
+
+# Activate the python venv
+source venv/bin/activate
+
+# Install requirements for this course
+pip install -r requirements.txt
+
+# Deactivate the venv
+deactivate
+
+# End script
